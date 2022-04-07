f004be2d701200ddc2301470061d719592096988
Mateo Oliver <mateooliver@MacBook-Air-de-Mateo.local>
2022-04-07 10:10:20 -0300

    responsive

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $("#calcular").click(function(){
         }else if (input >= 400) {
              var resultado=RIG.find((rig) =>{return rig.precio >=400});
     }
-         $("#resultado").prepend(` <div id=${resultado.id} class="d-flex col-3 mx-auto align-items-center" style="margin-top:2vh; height:8vh; font-family: 'Inter';
+         $("#resultado").prepend(` <div id=${resultado.id} class="d-flex ancho mx-auto align-items-center" style="margin-top:2vh; height:8vh; font-family: 'Inter';
             font-style: normal;font-weight: 300; background: #E2E2E2;box-shadow: 1% 3% 2%px rgba(0, 0, 0, 0.1);
             border-radius: 10px"><img class="mx-2" style="height:10px" src="../img/punto.png"> <span style="font-family: 'Inter';font-style: normal;font-weight: 700;">RIG </span> con ${resultado.cantidad} placa ${resultado.placa} </div>`)
               
@@ -84,18 +84,20 @@ $("#calcular").click(function(){
                 </tbody>
     
               </table>
-                <div class="col-9 mx-auto ">
+              </div>
+                <div class="resultado mx-auto ">
                     <div class="d-flex d-flex flex-row justify-content-between ">
-                        <div class="d-flex">
-                            <div class="text-center mx-1" style="height:30px; width:50px; padding:1%; background:#E61B80; border-radius:10px; border:none;"><img style="height:20px;" src="../img/compartir.png"></div>
-                            <div class="text-center mx-1" style="height:30px; width:50px; padding:1%; background:#E61B80; border-radius:10px; border:none;"><img style="height:20px;" src="../img/descargar.png"></div>
+                        <div class="d-flex my-auto">
+                            <div class="text-center mx-1 botonCompartir" ><img src="../img/compartir.png"></div>
+                            <div class="text-center mx-1 botonCompartir" ><img  src="../img/descargar.png"></div>
                         </div> 
-                        <div class="" style="width:17vw; text-align:center; ;background-color:#E6D21B; padding: 0 5% 0 5%; border-radius:20px;"> Comparar online</div>
-                        <div class="d-flex">
-                            <span style="font-family: 'Inter';font-style: normal;font-weight: 800;">Valor</span>
-                            <div class="col-12 d-flex" style="margin-left:10%; text-align:center; background:#E61B80; padding:0 5% 0 5%;border-radius:20px; color:white;" >$78.888</div>
+                        <div class="comprarOnline" > Comprar online</div>
+                        <div class="responsive precio">
+                            <span class="valor">Valor</span>
+                            <div class="backgroundPrecio">$78.888</div>
                         </div>
                     </div>
                 </div>
+                <div class="my-4" style="height:5px;"></div>
                 `);})
     });
